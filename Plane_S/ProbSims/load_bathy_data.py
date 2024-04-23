14f41bd9aa296f528c105d07ab11fcf8784ea543
MacaroniMutton <mananskher@gmail.com>
2024-04-23 23:32:24 +0530

    Fixed latitude longitude range problem

--- Plane_S/ProbSims/load_bathy_data.py
+++ Plane_S/ProbSims/load_bathy_data.py
@@ -9,6 +9,9 @@ ROWS = 96
 COLUMNS = 96
 
 data = xr.load_dataset('C:\\Users\Manan Kher\\OneDrive\\Documents\\MINI_PROJECT\\Plane-Crash-Bayesian-Search\\Plane_S\\ProbSims\\gebco_2023_n10.0063_s5.0845_w79.4883_e84.9023.nc')
+print(int(95*(data.sizes['lat']/96)))
+
+
 
 elevation = data.elevation
 li = np.array([[0]*COLUMNS for _ in range(ROWS)])
@@ -17,8 +20,8 @@ mini = float('inf')
 maxi = float('-inf')
 for lat in range(96):
     for lon in range(96):
-        lat_lng_li[95-lat][lon] = [[data.lat[(lat)*int(data.sizes['lat']/96)], data.lat[(lat+1)*int(data.sizes['lat']/96)]], [data.lon[(lon)*int(data.sizes['lon']/96)], data.lon[(lon+1)*int(data.sizes['lon']/96)]]]
-        li[95-lat][lon] = (elevation[(lat)*int(data.sizes['lat']/96):(lat+1)*int(data.sizes['lat']/96), (lon)*int(data.sizes['lon']/96):(lon+1)*int(data.sizes['lon']/96)].mean().load())
+        lat_lng_li[95-lat][lon] = [[data.lat[int(lat*(data.sizes['lat']/96))], data.lat[int((lat+1)*(data.sizes['lat']/96))-1]], [data.lon[int(lon*(data.sizes['lon']/96))], data.lon[int((lon+1)*(data.sizes['lon']/96))-1]]]
+        li[95-lat][lon] = (elevation[int(lat*(data.sizes['lat']/96)):int((lat+1)*(data.sizes['lat']/96))-1, int(lon*(data.sizes['lon']/96)):int((lon+1)*(data.sizes['lon']/96))-1].mean().load())
         mini = min(mini, li[95-lat][lon])
         maxi = max(maxi, li[95-lat][lon])
 lat_lng_li = np.array(lat_lng_li)
@@ -26,8 +29,7 @@ print(lat_lng_li)
 li = (li-mini)
 li = li/(maxi-mini)
 print(li)
-sns.heatmap(li)
-plt.show()
-# with open("test.txt", "wb") as fp:   #Pickling
-#     pickle.dump(li, fp)
+
+
+
 
